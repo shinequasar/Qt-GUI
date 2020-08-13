f5f369ef2406b06747f6ba085cde67dc1cb0f448
shinequasar <shinequasar@gmail.com>
2020-08-14 03:34:52 +0900

    [ADD] GUI vending machine program at Qt

---
+++ widget.cpp
@@ -0,0 +1,86 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    turnOut();
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+int money = 0;
+
+void Widget::turnOn(){
+    ui->pbcoffee->setEnabled(true);
+    ui->pbTea->setEnabled(true);
+    ui->pbMilk->setEnabled(true);
+}
+void Widget::turnOut(){
+    ui->pbcoffee->setEnabled(false);
+    ui->pbTea->setEnabled(false);
+    ui->pbMilk->setEnabled(false);
+}
+
+void Widget::lightManager(){
+    if(money>0) turnOn();
+    else turnOut();
+}
+
+void Widget::changeMoney(int price){
+    money += price;
+    ui->lcdNumber->display(money);
+    lightManager();
+}
+
+void Widget::subMoney(int price){
+    money -= price;
+    ui->lcdNumber->display(money);
+    lightManager();
+}
+
+void Widget::on_pb10_1_clicked()
+{
+    changeMoney(10);
+}
+
+void Widget::on_pb10_2_clicked()
+{
+    changeMoney(50);
+}
+
+void Widget::on_pb10_3_clicked()
+{
+    changeMoney(100);
+}
+
+void Widget::on_pb10_4_clicked()
+{
+    changeMoney(500);
+}
+
+void Widget::on_pbcoffee_clicked()
+{
+    subMoney(100);
+}
+
+void Widget::on_pbTea_clicked()
+{
+    subMoney(150);
+}
+
+void Widget::on_pbMilk_clicked()
+{
+    subMoney(200);
+}
+
+void Widget::on_pbReset_clicked()
+{
+    money=0;
+    ui->lcdNumber->display(money);
+}
---
+++ widget.h
@@ -0,0 +1,43 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+    int money {0};
+    void changeMoney(int diff);
+    void subMoney(int diff);
+    void turnOn();
+    void turnOut();
+    void lightManager();
+private slots:
+    void on_pb10_1_clicked();
+
+    void on_pb10_2_clicked();
+
+    void on_pb10_3_clicked();
+
+    void on_pb10_4_clicked();
+
+    void on_pbcoffee_clicked();
+
+    void on_pbTea_clicked();
+
+    void on_pbMilk_clicked();
+
+    void on_pbReset_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
